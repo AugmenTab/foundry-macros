aa74c3eb13156ff799a50c8513eccedd4c4bf79e
AugmenTab <tbaum92@live.com>
2021-06-15 10:41:18 -0600

    Switched deprecated .entities to .contents.

--- tsl/new-hex.js
+++ tsl/new-hex.js
@@ -32,10 +32,10 @@ async function buildMessageTable() {
 // Construct the chat message.
 async function buildChatData() {
   return {
-    user: game.user._id,
+    user: game.user.id,
     speaker: ChatMessage.getSpeaker(),
     content: await buildMessageTable(),
-    whisper: game.users.entities.filter(u => u.isGM).map(u => u._id)  
+    whisper: game.users.contents.filter(u => u.isGM).map(u => u.id)  
   };
 };
 
