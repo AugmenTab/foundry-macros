04fd09fe10d1be18b54cc005e67bf3dd2c60eb04
AugmenTab <tbaum92@live.com>
2021-09-18 10:06:14 -0600

    Added mimic gun rangefinder macro.

---
+++ lancer/mimic-gun.js
@@ -0,0 +1,31 @@
+// Calculate a Range for the Mimic Gun.
+async function getRange() {
+  const roll = await new Roll("d20").roll({ async: true });
+  const text =
+  `<i class="cci cci-range i--m i--dark"></i> ${roll.total}` +
+  `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;::&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;` +
+  `${Math.floor(roll.total / 2) + 1} ` +
+  `<i class="cci cci-kinetic i--m damage--kinetic"></i>`;
+  return text;
+}
+
+// Construct HTML message table for chat message.
+async function buildMessageTable() {
+  const messageTable =  
+    "<b><h2>Mimic Gun Ranges</h2></b>" + 
+    "<p><b>Range 1: </b>" + await getRange() +
+    "<hr><p><b>Range 2: </b>" + await getRange() +
+    "<hr><p><b>Range 3: </b>" + await getRange()
+  return messageTable;
+}
+
+// Construct the chat message.
+async function buildChatData() {
+  return {
+    user: game.user.id,
+    speaker: ChatMessage.getSpeaker(),
+    content: await buildMessageTable()
+  };
+}
+
+ChatMessage.create(await buildChatData(), {});
