c06bc9025ee276e6a6d83038d41188249d0ab46b
AugmenTab <tbaum92@live.com>
2021-09-19 15:04:15 -0600

    Wild surge macro now whispers result.

--- tsl/wild-surge.js
+++ tsl/wild-surge.js
@@ -1,6 +1,14 @@
 // Roll Wild Surge
 async function getWildSurge() {
-  const result = game.tables.contents.find(t => t.name === "Wild Surges").draw();
+  const table = game.tables.contents.find(t => t.name === "Wild Surges");
+  const draw = await table.draw({ displayChat: false, async: true });
+  console.log(draw);
+  ChatMessage.create({
+    user: game.user.id,
+    speaker: ChatMessage.getSpeaker(),
+    content: `<h2>Wild Surge</h2><p>${await draw.results[0].data.text}</p>`,
+    whisper: game.users.contents.filter(u => u.isGM).map(u => u.id)
+  }, {});
 };
 
 await getWildSurge();
