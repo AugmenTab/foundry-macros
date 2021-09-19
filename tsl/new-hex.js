cf9f12c6728688c8965c32eb98b5d15df36c1f99
AugmenTab <tbaum92@live.com>
2021-09-19 12:57:57 -0600

    Fixed bug where some terrains came up undefined.

--- tsl/new-hex.js
+++ tsl/new-hex.js
@@ -18,14 +18,31 @@ async function getEsotericsResults() {
 
 // Roll weather event.
 async function getWeatherResults(terrain) {
-  return "TODO";
+  const table = game.tables.contents.find(t => t.name === "Weather Events");
+  const draw = await table.draw({ displayChat: false, async: true });
+  const result = draw.results[0].text;
+  // console.log(draw.results[0]);
+  return result;
+  // if (terrain.includes("Black Sand") && result.includes("Flaywind Storm")) {
+  //   return "@JournalEntry[e8x3hdXbx8NG8AJ7]{Necrotic Flaywind}";
+  // } else {
+  //   return result;
+  // }
 };
 
 // Roll terrain event.
 async function getTerrainResults() {
   const table = game.tables.contents.find(t => t.name === "Terrain Events");
   const draw = await table.draw({ displayChat: false, async: true });
-  return draw.results[0].text;
+  const result = draw.results[0].text;
+  if (result) {
+    return result;
+  } else {
+    const title = draw.results[0].data.text;
+    const journal = game.journal.contents.find(j => j.name === title);
+    console.log(journal);
+    return `@JournalEntry[${journal.id}]{${journal.name}}`;
+  }
 };
 
 // Roll encounter event.
@@ -37,7 +54,7 @@ async function getEncounterEvent() {
 const esoterics = await getEsotericsResults();
 const terrain = await getTerrainResults();
 const weather = await getWeatherResults(terrain);
-const encounter = await getEncounterEvent();
+const encounter = "TODO"; //await getEncounterEvent();
 async function buildMessageTable() {
   const messageTable = `
     <b><h2>New Day or Hex</h2></b>
